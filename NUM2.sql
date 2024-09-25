179b5b0be165f3df323b70303cd9f68fb5d99ff1
Gulin-dev <GulinEgorEvgenevich@gmail.com>
2024-09-25 18:49:30 +0300

    Update NUM2.sql

--- NUM2.sql
+++ NUM2.sql
@@ -1,1 +1,7 @@
-
+SELECT
+    age_group as 'Возрастная группа',
+    ROUND(AVG(flight_time), 2) AS 'Среднее время'
+FROM passengers
+JOIN fly_mapping ON passengers.id = fly_mapping.passenger_id
+JOIN flying ON fly_mapping.flying_id = flying.id
+GROUP BY age_group;
