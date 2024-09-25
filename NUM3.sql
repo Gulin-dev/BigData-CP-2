30c3f26fed6e031bfb2f60fb94f7fdc2acb49a8d
Gulin-dev <GulinEgorEvgenevich@gmail.com>
2024-09-25 19:23:37 +0300

    Update NUM3.sql

--- NUM3.sql
+++ NUM3.sql
@@ -1,10 +1,17 @@
-SELECT 
+SELECT
+    --Выводим Имя пассажира
     passengers.name,
+    --Выводим Счёт количества полётов
     COUNT(fly_mapping.flying_id) AS flight_count
+    --Откуда выводим
 FROM passengers
+    --Присоединяем таблицу fly_mapping
 JOIN fly_mapping ON passengers.id = fly_mapping.passenger_id
+    --Сортируем
 GROUP BY passengers.id, passengers.name
+    --Делаем фильтрацию
 HAVING 
+    --Счётчик
     COUNT(fly_mapping.flying_id) =
   (
         SELECT MAX(flight_count)
