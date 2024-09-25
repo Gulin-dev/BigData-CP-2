f375a373d8c0d783fa16898561bfdbf8169b564f
Gulin-dev <GulinEgorEvgenevich@gmail.com>
2024-09-25 19:06:11 +0300

    Create NUM3.sql

---
+++ NUM3.sql
@@ -0,0 +1,17 @@
+SELECT 
+    passengers.name,
+    COUNT(fly_mapping.flying_id) AS flight_count
+FROM passengers
+JOIN fly_mapping ON passengers.id = fly_mapping.passenger_id
+GROUP BY passengers.id, passengers.name
+HAVING 
+    COUNT(fly_mapping.flying_id) =
+  (
+        SELECT MAX(flight_count)
+        FROM 
+        (
+            SELECT COUNT(flying_id) AS flight_count
+            FROM fly_mapping
+            GROUP BY passenger_id
+        ) AS subquery
+    );
