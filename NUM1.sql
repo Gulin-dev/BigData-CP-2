2f9312ad51a8893ea13da0bea75f1a887aa4aa9d
Gulin-dev <GulinEgorEvgenevich@gmail.com>
2024-09-25 18:44:26 +0300

    Update NUM1.sql

--- NUM1.sql
+++ NUM1.sql
@@ -1,8 +1,7 @@
 SELECT 
-    passengers.name AS passenger_name,
-    SUM(flying.price) AS total_spent
+    passengers.name AS 'Имя пассажира',
+    SUM(flying.price) AS 'Общие траты на полёты'
 FROM passengers
 JOIN fly_mapping ON passengers.id = fly_mapping.passenger_id
 JOIN flying ON fly_mapping.flying_id = flying.id
 GROUP BY passengers.id, passengers.name
-ORDER BY total_spent DESC;
