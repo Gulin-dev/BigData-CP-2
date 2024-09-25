05b84dae7671b2ab8e647ea3bede9a9e11505c48
Gulin-dev <GulinEgorEvgenevich@gmail.com>
2024-09-25 19:29:56 +0300

    Update NUM5.sql

--- NUM5.sql
+++ NUM5.sql
@@ -1,15 +1,19 @@
-SELECT 
-    CASE 
+SELECT
+    --Кейс по таблице origin
+    CASE
         WHEN flying.point_from < flying.point_to THEN flying.point_from
         ELSE flying.point_to
     END AS origin,
+    --Кейс по таблице destination
     CASE 
         WHEN flying.point_from < flying.point_to THEN flying.point_to
         ELSE flying.point_from
     END AS destination,
+    --Счётчик
     COUNT(fly_mapping.id) AS flight_count
-FROM 
-    flying
-JOIN 
-    fly_mapping ON flying.id = fly_mapping.flying_id
+    -- Откуда выводим
+FROM flying
+    --Присоединяем таблицу fly_mapping
+JOIN fly_mapping ON flying.id = fly_mapping.flying_id
+    --Сортируем
 GROUP BY origin, destination
